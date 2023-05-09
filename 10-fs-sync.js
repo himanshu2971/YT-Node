dcc979ae55f68718a02350a84102d5930f6b8cf2
Jarvis <himanshushourabh@gmail.com>
2023-05-09 20:53:30 +0530

    Upto HTTP

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,12 +1,15 @@
 const {readFileSync, writeFileSync}= require('fs')
-
+console.log("start")
 const first=readFileSync('./content/first.txt','utf8')
 const second=readFileSync('./content/second.txt','utf8')
 
-console.log(first)
-console.log(second)
+// console.log(first)
+// console.log(second)
 
 writeFileSync(
   "./content/result-sync.txt",
   `Here is the result: ${first}, ${second}`
 );
+
+console.log('done with the task')
+console.log('starting the next one')
